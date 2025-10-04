d99e21c8543ccc5bf5cf55741594dda43f64ed4e
Hardcodepunk <howl@hardcodepunk.be>
2025-10-04 09:12:36 +0200

    correct date

--- components/Sections/SectionEvents/Events/constantsEvents.tsx
+++ components/Sections/SectionEvents/Events/constantsEvents.tsx
@@ -3,7 +3,7 @@ export const constantsEvents = [
     title: 'Autumn Camp',
     location: 'Blaarmeersen, Gent',
     price: '85€',
-    date: '24 - 24 October',
+    date: '20 - 24 October',
     description:
       'A week-long inline skate camp in autumn. The camps are held in the Blaarmeersen skatepark in Gent. Open to inline skaters of all ages and skill levels. Led by Ryan who teaches trick focused inline skating or aggressive inline skating. A great way to learn new skills, make new friends, and have a great time on wheels. Only 10 spots available. When raining we have arranged SBA indoor skatepark as backup, it will be €5 extra per person to enter.',
     buttonLabel: 'Register',
