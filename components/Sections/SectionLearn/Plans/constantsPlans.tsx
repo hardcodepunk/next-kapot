b66a759202881a987007dab5bf0b958aff99c8af
Hardcodepunk <howl@hardcodepunk.be>
2026-01-15 16:59:07 +0100

    Remove kapot member from plans

--- components/Sections/SectionLearn/Plans/constantsPlans.tsx
+++ components/Sections/SectionLearn/Plans/constantsPlans.tsx
@@ -17,20 +17,20 @@ export const constantsPlans = [
     buttonLink: 'https://calendly.com/kapot/private-session',
     buttonLabel: 'Book session',
   },
-  {
-    title: 'Kapot Member',
-    price: '€400 (first 5 members) per year',
-    description:
-      'One year access to a coach: 24/7 feedback. We will analyze your videos, suggest training plans and answer questions. One monthly one-hour feedback session included, online or in person. This plan saves you €80 if you would want a private session every month.',
-    list: [
-      'monthly 1h private session (international member 1h live call)',
-      'one year 24/7 online feedback',
-      'on and off skate training advice and plans',
-      'access to our online community',
-      'submit your clips, instant feedback',
-      'kapot shirt',
-    ],
-    buttonLink: 'https://buy.stripe.com/00gfZDeaR55p4V28wy',
-    buttonLabel: 'Become member',
-  },
+  // {
+  //   title: 'Kapot Member',
+  //   price: '€400 (first 5 members) per year',
+  //   description:
+  //     'One year access to a coach: 24/7 feedback. We will analyze your videos, suggest training plans and answer questions. One monthly one-hour feedback session included, online or in person. This plan saves you €80 if you would want a private session every month.',
+  //   list: [
+  //     'monthly 1h private session (international member 1h live call)',
+  //     'one year 24/7 online feedback',
+  //     'on and off skate training advice and plans',
+  //     'access to our online community',
+  //     'submit your clips, instant feedback',
+  //     'kapot shirt',
+  //   ],
+  //   buttonLink: 'https://buy.stripe.com/00gfZDeaR55p4V28wy',
+  //   buttonLabel: 'Become member',
+  // },
 ]
