2c7078dfcaa282e974fd0ee049ee71b2f4408290
Hardcodepunk <howl@hardcodepunk.be>
2026-05-21 15:26:20 +0200

    fix: close unclosed && blocks in styled components

--- components/Sections/styles.tsx
+++ components/Sections/styles.tsx
@@ -257,19 +257,21 @@ export const StyledSplashIllustrationBottomVariation1 = styled(Image)(
 export const SectionFooter = styled('footer')(
   ({ theme }) => `
     && {
-    position: relative;
-    background: ${theme.palette.primary.main};
-    padding: 70px 0 140px;
+      position: relative;
+      background: ${theme.palette.primary.main};
+      padding: 70px 0 140px;
+    }
   `,
 )
 
 export const StyledLinkIcon = styled(Link)(
   ({ theme }) => `
     && {
-    color: ${theme.palette.common.black};
-    display: flex;
-    transition: all 0.4s ease-out;
-    margin: 6px;
+      color: ${theme.palette.common.black};
+      display: flex;
+      transition: all 0.4s ease-out;
+      margin: 6px;
+    }
   `,
 )
 
