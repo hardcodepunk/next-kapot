3463f3381c5ebbdcbe0a7a7738daa2b9f1fe42cf
Hardcodepunk <howl@hardcodepunk.be>
2026-05-21 16:46:29 +0200

    refactor: type Button wrapper props

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,9 +1,17 @@
 // Modules
-import { memo } from 'react'
+import { MouseEventHandler, ReactNode, memo } from 'react'
+
+// MUI
+import { ButtonProps } from '@mui/material'
 
 // Styles
 import { StyledButton } from './styles'
 
-const Button = ({ label, ...rest }) => <StyledButton {...rest}>{label}</StyledButton>
+interface IButton extends Omit<ButtonProps, 'children' | 'onClick'> {
+  label: ReactNode
+  onClick?: MouseEventHandler<HTMLElement>
+}
+
+const Button = ({ label, ...rest }: IButton) => <StyledButton {...rest}>{label}</StyledButton>
 
 export default memo(Button)
